d0b355ac3126b6c5fb235186b503745a1e848204
ICT ACADEMY <yazid>
2023-05-22 16:57:51 +0530

    f

---
+++ reverese.js
@@ -0,0 +1,13 @@
+
+
+function reverseString(str) {
+
+    // empty string
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+const result = reverseString("yazid");
+console.log(result);
