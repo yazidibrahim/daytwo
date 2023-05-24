a9d23f8749f7a2cb154358f09d9acb36d29fdfb3
yazidibrahim <83606566+yazidibrahim@users.noreply.github.com>
2023-05-24 11:55:03 +0530

    Update kd.js

--- kd.js
+++ kd.js
@@ -1,4 +1,4 @@
-let districts=[
+  let districts=[
     {
         id:"1",
         title:"TRIVANDRUM",
@@ -10,81 +10,79 @@ let districts=[
         id:"2",
         title:"KOLLAM",
         description:"Known for its backwaters",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Scenic_beauty_of_Kollam_District.jpg/250px-Scenic_beauty_of_Kollam_District.jpg"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/RP_Mall%2C_Kollam.jpg/1024px-RP_Mall%2C_Kollam.jpg"
     },
     {
         id:"3",
         title:"Pathanamthitta",
         description:"Known for its traditional arts and crafts",
-        image:"b"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Gavi%2C_Kerala.jpg/119px-Gavi%2C_Kerala.jpg"
     },
     {
         id:"4",
         title:"ALAPPUZHA",
         description:"Famous for its boat races",
-        image:"b"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Alappuzha_Boat_Beauty_W.jpg/288px-Alappuzha_Boat_Beauty_W.jpg"
     },
     {
         id:"5",
         title:"Kottayam",
         description:"Known as the “Land of Letters, Lakes, and Latex" ,
-        image:"htt"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vembanad_Lake_at_Kumarakom.jpg/225px-Vembanad_Lake_at_Kumarakom.jpg"
     },
     {
         id:"6",
         title:"Idukki",
         description:"Has a large population of tribal communities, with many preserving their traditional way of life",
-        image:"https://ww"
+        image:"https://upload.wikimedia.org/wikipedia/commons/7/72/Idukki009.jpg"
     },
     {
         id:"7",
         title:"Ernakulam",
         description:"GOT RICH HISTORY",
-        image:"ht"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/INS_Vikrant_under_construction_at_Cochin_Shipyard.png/250px-INS_Vikrant_under_construction_at_Cochin_Shipyard.png"
     },
     {
         id:"8",
         title:"THRISSUR",
         description:"FAMOUS FOR ITS FESTIVAL",
-        image:"https://www.google"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/JJP_112.jpg/243px-JJP_112.jpg"
     },
     {
         id:"9",
         title:"PALAKKAD",
         description:"RICH HERITAGE CULTURE",
-        image:"https://www.go"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/KSRTC_Bus_Terminal_Palakkad.jpg/1024px-KSRTC_Bus_Terminal_Palakkad.jpg"
     },
     {
         id:"10",
         title:"MALAPPURAM",
         description:"SCENIC BEAUTY",
-        image:"https://w"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Malappuram_DownHill_Aerial_View.jpg/250px-Malappuram_DownHill_Aerial_View.jpg"
     },
     {
         id:"11",
         title:"KOZHIKODE",
         description:"KNOWN FOR CUISINE",
-        image:"https:/"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Calicut_mini_bypass.jpg/1024px-Calicut_mini_bypass.jpg"
     },
     {
         id:"12",
         title:"WAYANAD",
         description:"FOREST AREA",
-        image:"https://www.google.com/search?channel=fs&client=ubuntu-sn&q=trivandrum#"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Blue%2C_Green_%26_White.jpg/250px-Blue%2C_Green_%26_White.jpg"
     },
     {
         id:"13",
         title:"KANNUR",
         description:"KNOWN FOR BEAUTIFUL BEACHES",
-        image:"https://www.google.com/search?channel=fs&client=ubuntu-sn&q=trivandrum#"
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Payyambalam_Beach_Back_Water.jpg/1024px-Payyambalam_Beach_Back_Water.jpg"
     },
     {
         id:"14",
         title:"KASARGOD",
         description:"UNIQUE CULTURE",
-        image:"https://www.google.com/search?channel=fs&client=ubuntu-sn&q=trivandrum#"
-    },
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Chandragiri_River.jpg/1024px-Chandragiri_River.jpg"
+    }
 
 ]
-let filteration=districts.filter(e=>e.id==='1')
-console.log(filteration)
